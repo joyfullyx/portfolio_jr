17911edf09c64b926079f93327e04bf5145e94fb
Joy Rhee <joyfullyx@gmail.com>
2021-06-09 15:46:31 -0700

    inital files, component skeletons

---
+++ src/components/Contact/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact content goes here</h1>
+        </div>
+    )
+}
---
+++ src/components/Intro/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Intro() {
+    return (
+        <div>
+            <h1>Bio/intro content goes here</h1>
+        </div>
+    )
+}
