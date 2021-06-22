f150203bafe44040b5d06082a26e41b6c6e9985a
Joy Rhee <joyfullyx@gmail.com>
2021-06-22 15:00:05 -0700

    add links to contact icon, some bio text

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import { Box } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
+import { LinkedIn, GitHub, Email } from "@material-ui/icons";
 
 export default function Contact() {
   return (
     <div>
       <Box display="flex" justifyContent="center">
-        <h1>Contact content goes here</h1>
+        {/* To LinkedIn */}
+        <IconButton onClick={() => window.open('https://www.linkedin.com/in/joy-rhee/', '_blank')}>
+          <LinkedIn fontSize="large" />
+        </IconButton>
+
+        {/* To Github */}
+        <IconButton onClick={() => window.open('https://github.com/joyfullyx', '_blank')}>
+          <GitHub fontSize="large" />
+        </IconButton>
+
+        {/* To email */}
+        <a href="mailto:joyfullyx@gmail.com">
+          <IconButton>
+            <Email fontSize="large" />
+          </IconButton>
+        </a>
       </Box>
     </div>
   );
--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,13 +1,35 @@
 import React from "react";
 import { Box } from "@material-ui/core";
 import { Container } from "@material-ui/core";
+import { Paper } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    ...theme.typography.button,
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(2),
+  },
+}));
 
 export default function Intro() {
+
+  const classes = useStyles();
+
   return (
     <div>
-      <Box display="flex" justifyContent="center">
-        <p>Hey there! I'm Joy, a software developer based in the greater Seattle area. </p>
-      </Box>
+      <Container>
+        <Paper elevation={0}>
+          <Box display="flex" justifyContent="center" width="100%">
+            <div className={classes.root}>
+              <p>Hey there! I'm Joy, a professional musician turned software developer based in the greater Seattle area. I love a good challenge and getting hands on with building and creating products that are simple, elegant, and beautiful. 
+             <br></br>
+              Take a look around and feel free to contact me through email or connect on LinkedIn!
+              </p>
+            </div>
+          </Box>
+        </Paper>
+      </Container>
     </div>
   );
 }
